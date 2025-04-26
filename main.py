bb605b46daaa83427acf160a6c42813afd3b6c7c
Sahil <sahilsharma2000dtc@gmail.com>
2025-04-26 15:26:09 +0530

    streamlit selectbox

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ st.text("Welcome to streamlit application with python, and interactive app")
 
 st.write("Choose your favourite game")
 
+
+select_box = st.selectbox("Your favourite coffe: ", ["Cold Coffee", "Iced Latte", "Iced Americano"])
+st.write(f'You choose {select_box} it was a excellent choice!')
+st.success("you choice have been saved to the servers....")
+
+st.write("Choose your favourite programming language")
+programmin_language = st.selectbox("Your language", ["JavaScript", "Java", "Python", "C++", "C#", "Erlang"])
+if programmin_language != "":
+    st.write(f'you selected {programmin_language}, it is a great choice.')
+    st.success("Yooohoooo!!!")
